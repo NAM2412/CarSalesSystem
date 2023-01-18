568f83aa07e42ee68dc7c64ecb1740217884beaf
NAM2412 <20520643@gm.uit.edu.vn>
2023-01-18 20:03:27 +0700

    Update SQL

--- Model/Model1.Designer.cs
+++ Model/Model1.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\UIT\Tài Liệu Học Tập\Năm 3\Kì 1\OOAD\Project\CarSalesSystem\Model\Model1.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\UIT\Tài Liệu Học Tập\Năm 3\Kì 1\OOAD\Project\CarSale\Model\Model1.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
