34dd3db7bb1dfc5aed61c8232ce3c7f111012c2e
NAM2412 <20520643@gm.uit.edu.vn>
2023-01-27 23:29:03 +0700

    On Statistic Funtion (not done)

--- Admin/Pages/Dashboard.xaml.cs
+++ Admin/Pages/Dashboard.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CarSalesSystem.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,14 +21,37 @@ namespace CarSalesSystem.Admin.Pages
     /// </summary>
     public partial class Dashboard : Page
     {
+        PRODUCT product;
         public Dashboard()
         {
             InitializeComponent();
-        }
+            
+            cardBugatti.SaleNumber = NumberCarSale("P001").ToString();
+            cardBugatti.Profit = String.Format("{0:0,0}", Calculateprofit("P001"));
 
-        private void CarCard_Loaded(object sender, RoutedEventArgs e)
+            cardMaserati.SaleNumber = NumberCarSale("P002").ToString();
+            cardMaserati.Profit = String.Format("{0:0,0}", Calculateprofit("P002"));
+
+            cardToyota.SaleNumber = NumberCarSale("P003").ToString();
+            cardToyota.Profit = String.Format("{0:0,0}", Calculateprofit("P003"));
+
+            cardTesla.SaleNumber = NumberCarSale("P004").ToString();
+            cardTesla.Profit = String.Format("{0:0,0}", Calculateprofit("P004"));
+
+            cardLexus.SaleNumber = NumberCarSale("P006").ToString();
+            cardLexus.Profit = String.Format("{0:0,0}", Calculateprofit("P006"));
+
+        }
+        private int NumberCarSale(string carID)
         {
-
+            return DataProvider.Ins.DB.ORDERBILLs.Where(x => x.PRO_ID == carID && x.OB_DATEB <= DateTime.Now).Count();
         }
+        private decimal Calculateprofit(string carID)
+        {
+            product = DataProvider.Ins.DB.PRODUCTs.Where(x => x.PRO_ID.Equals(carID)).FirstOrDefault();
+            decimal revenue = product.PRICE*NumberCarSale(product.PRO_ID);
+            return revenue + (revenue*30)/100;
+        }
+
     }
 }
