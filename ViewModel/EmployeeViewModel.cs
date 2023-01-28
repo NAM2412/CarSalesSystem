8b21520ee59c9bff92c6ee4e9708913f45a1021d
baonehe <95235910+baonehe@users.noreply.github.com>
2023-01-29 00:19:29 +0700

    Ui Employee

---
+++ ViewModel/EmployeeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using CarSalesSystem.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarSalesSystem.ViewModel
+{
+    public class EmployeeViewModel: BaseViewModel, INotifyPropertyChanged
+    {
+    }
+}
