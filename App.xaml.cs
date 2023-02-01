b96879d91331bda5336e4a53a2289ce0b78bfc70
vulekhanh <vulekhanh69@gmail.com>
2023-02-01 09:10:04 +0700

    set  phonebox to number only and change sql query in fillingInformation.xaml

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Configuration;
 using System.Data;
 using System.Linq;
